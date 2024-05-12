4acfa46a03734bff6dcb5fed8fd5b3b142f99c47
Tulat2002 <126972147+Tulat2002@users.noreply.github.com>
2024-05-12 13:55:00 +0700

    delete user

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -14,10 +14,16 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/user/create")
+    @PostMapping("/user")
     public User createNewUser(@RequestBody User postManUser) {
         User user = this.userService.handleCreateUser(postManUser);
         return user;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
+        return "Deleted user successfully";
     }
 
 }
